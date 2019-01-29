3b219930cf16e5415a838997eb873abe6e8ec02c
johnbonda <john@belfricsbt.com>
2019-01-29 15:49:34 +0530

    First commit

--- init.js
+++ init.js
@@ -1,16 +1,12 @@
 module.exports = async function () {
   console.log('enter dapp init')
 
-  app.registerContract(1000, 'bst.transfer')
-  app.registerContract(1001, 'bst.transferFrom')
-  app.registerContract(1002, 'bst.approve')
-  app.registerContract(1003, 'bst.testMint')
+  app.registerContract(1000, 'aatd.generateOneTimeDAppAddress')
+  app.registerContract(1001, 'aatd.transfer')
 
 
   app.registerFee(1000, '0', 'BEL')
   app.registerFee(1001, '0', 'BEL')
-  app.registerFee(1002, '0', 'BEL')
-  app.registerFee(1003, '0', 'BEL')
   
 
 
